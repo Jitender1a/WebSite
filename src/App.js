df99cb75eb2c0d632a6d5668d1ffd9fbf42620a2
Jitender Singh <jitendersingh@Jitenders-MacBook-Pro.local>
2019-01-20 17:00:49 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Service from './components/Service'
+import Work from './components/Work'
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/Contact' component={Contact}/>
+        <Route path='/About' component={About}/>
+        <Route path='/Service' component={Service}/>
+        <Route path='/Work' component={Work}/>
+        </Switch>
       </div>
     );
   }
